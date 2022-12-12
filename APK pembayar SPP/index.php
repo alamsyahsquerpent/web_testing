78c5858938994c8950390d9434e04a927edecc26
alamsyahsquerpent <102565874+alamsyahsquerpent@users.noreply.github.com>
2022-12-12 16:21:29 +0700

    Add files via upload

---
+++ APK pembayar SPP/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <titile>Aplikasi Pembayaran SPP</title>
+</head>
+<body>
+    <div>
+        <h1>Silahkan Masuk</4>
+        <form method="post">
+            <label for="username">Username</label>
+            <input type="text" name="username" id="username">
+            <label for="password">Passowrd</label>  
+            <input type="password" name="password" id="password"><br> 
+            <input type="submit" name="masuk" value="masuk">
+        </form>
+    </div>
+</body>
+</html>
